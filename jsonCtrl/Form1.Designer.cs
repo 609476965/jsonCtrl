b4ca51a8de6193f8f2abdfedda696755e519f044
609476965 <yuyongshen2006@hotmail.com>
2017-11-22 18:14:49 +0800

    jsonCtrl

---
+++ jsonCtrl/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace jsonCtrl {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.服务器名称 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.服务器路径 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.服务器状态 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Location = new System.Drawing.Point(383, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 313);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "启动";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.服务器名称,
+            this.服务器路径,
+            this.服务器状态,
+            this.columnHeader1});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(377, 313);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // 服务器名称
+            // 
+            this.服务器名称.Text = "服务器名称";
+            this.服务器名称.Width = 94;
+            // 
+            // 服务器路径
+            // 
+            this.服务器路径.Text = "服务器路径";
+            this.服务器路径.Width = 98;
+            // 
+            // 服务器状态
+            // 
+            this.服务器状态.Text = "服务器状态";
+            this.服务器状态.Width = 88;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "执行程序";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 313);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader 服务器名称;
+        private System.Windows.Forms.ColumnHeader 服务器路径;
+        private System.Windows.Forms.ColumnHeader 服务器状态;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ jsonCtrl/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace jsonCtrl {
+    public enum stateType {
+        close,
+        open,
+        exist
+    }
+    public partial class Form1 : Form {
+        public string path;
+        public string bsd = "BattleServerD.exe";
+        public string dbcd = "DBCacheD.exe";
+        public string gsd = "GameServerD.exe";
+        public string gd = "GateD.exe";
+        public string wsd = "WorldServerD.exe";
+        public Dictionary<int, List<Process>> group = new Dictionary<int, List<Process>>();
+        public stateType flag = stateType.close;
+        public bool switchFlag = false;
+        public string startPath = Application.StartupPath;
+        
+
+
+        public Form1() {
+            InitializeComponent();
+            group.Clear();
+            timer1.Start();
+            path = Application.StartupPath + "/config.json";
+            if (File.Exists(path) == false) {
+                var a = File.Open(path, FileMode.OpenOrCreate);
+
+                string jsonTest = "{ \"1\":{\"id\":1,\"name\":\"吃鱼测试\",\"path\":\"/bin_eatgame/\",\"exeList\":[\"BattleServerD\",\"DBCacheD\",\"GameServerD\",\"GateD\",\"WorldServerD\"] },\"2\":{\"id\":2,\"name\":\"抛圈测试\",\"path\":\"/bin_throwgame/\",\"exeList\":[\"BattleServerD\",\"DBCacheD\",\"GameServerD\",\"GateD\",\"WorldServerD\"]}}";
+                StreamWriter sw = new StreamWriter(a);
+                sw.Write(jsonTest);
+                //清空缓冲区
+                sw.Flush();
+                //关闭流
+                sw.Close();
+
+                a.Close();
+            }
+            JsonReader();
+        }
+
+        [STAThread]
+        public void JsonReader() {
+            var s = File.ReadAllText(path);
+            //richTextBox1.Text = s;
+            JObject c = (JObject)JsonConvert.DeserializeObject(s);
+            listView1.BeginUpdate();
+            //Process[] pcs = Process.GetProcesses();
+            foreach (var a in c) {
+                var name = a.Value["name"];
+                var path = a.Value["path"];
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = name.ToString();
+                lvi.SubItems.Add(path.ToString());
+                lvi.SubItems.Add("关闭");
+                lvi.SubItems.Add(a.Value["exeList"].ToString());
+                listView1.Items.Add(lvi);
+                List<Process> proc = new List<Process>();
+                JArray ja = (JArray)JsonConvert.DeserializeObject(a.Value["exeList"].ToString());
+                for (int j = 0; j < ja.Count; j++) {
+                    try {
+                        string strUpPath = startPath.Substring(0, startPath.LastIndexOf("\\"));
+                        string str = strUpPath + path.ToString() + ja[j].ToString();
+                        strUpPath = strUpPath.Replace("\\", "/");
+                        str = str.Replace("\\", "/");
+                        Process[] app = Process.GetProcessesByName(ja[j].ToString());
+                        if (app.Length > 0) {
+                            for (int l = 0; l < app.Length; l++) {
+                                string fName = app[l].MainModule.FileName.Replace("\\", "/");
+                                if (fName == str + ".exe") {
+                                    app[l].StartInfo.FileName = str;
+                                    app[l].StartInfo.WorkingDirectory = strUpPath + path.ToString();
+                                    proc.Add(app[l]);
+                                }
+                            }
+                        }
+                        //string fName = p.MainModule.FileName;
+                        //if (fName == startPath + path.ToString() + ja[j]) {
+                        //    proc.Add(p.Id);
+                        //    listView1.Items[lvi.Index].SubItems[2].Text = "已启动";
+                        //    break;
+                        //}
+                    }
+                    catch (Exception e) {
+
+                    }
+                }
+                if (proc.Count > 0) {
+                    listView1.Items[lvi.Index].SubItems[2].Text = "已启动";
+                    group.Add(lvi.Index, proc);
+                }
+            }
+            listView1.EndUpdate();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            int i = 0;
+            foreach (ListViewItem lvi in listView1.SelectedItems) {
+                i = lvi.Index;
+
+                string pathStr = listView1.Items[lvi.Index].SubItems[1].Text;
+                string exeList = listView1.Items[lvi.Index].SubItems[3].Text;
+                JArray ja = (JArray)JsonConvert.DeserializeObject(exeList);
+                string str = listView1.Items[i].SubItems[2].Text;
+
+                switch (str) {
+                    case "已启动":
+                        // flag = stateType.close;
+                        listView1.Items[i].SubItems[2].Text = "关闭";
+                        switchFlag = false;
+                        break;
+                    case "关闭":
+                        if (!group.ContainsKey(i)) {
+                            List<Process> proc = new List<Process>();
+                            string strUpPath = startPath.Substring(0, startPath.LastIndexOf("\\"));
+                            strUpPath = strUpPath.Replace("\\", "/");
+                            for (int j = 0; j < ja.Count; j++) {
+                                Process pj = new Process();
+                                pj.StartInfo.FileName = strUpPath + pathStr + ja[j];
+                                pj.StartInfo.WorkingDirectory = strUpPath + pathStr;
+                                //pj.Start();
+                                proc.Add(pj);
+                            }
+                            group.Add(i, proc);
+                        }
+                        //if (group[i].Count > 0) break;
+
+                        // flag = stateType.open;
+                        listView1.Items[i].SubItems[2].Text = "已启动";
+                        switchFlag = true;
+                        break;
+                }
+                foreach (var a in group[i]) {
+                    if (listView1.Items[i].SubItems[2].Text == "关闭")
+                        a.Kill();
+                    else if (listView1.Items[i].SubItems[2].Text == "已启动")
+                        a.Start();
+                }
+                //using (StreamWriter writer = new StreamWriter(openFileDialog1.FileName)) {
+                //    writer.Write(this.richTextBox1.Text);
+                //    writer.Close();
+                //    JObject c = (JObject)JsonConvert.DeserializeObject(File.ReadAllText(path));
+                //    MessageBox.Show(c["1"]["path"].ToString());
+                //}
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            foreach (ListViewItem lvi in listView1.SelectedItems) {
+                if (lvi.SubItems[2].Text == "关闭")
+                    button1.Text = "开启";
+                else
+                    button1.Text = "关闭";
+            }
+            JArray ja = (JArray)JsonConvert.DeserializeObject(a.Value["exeList"].ToString());
+            for (int j = 0; j < ja.Count; j++) {
+                try {
+                    string strUpPath = "./" + startPath;
+                    string str = strUpPath + path.ToString() + ja[j].ToString();
+                    strUpPath = strUpPath.Replace("\\", "/");
+                    str = str.Replace("\\", "/");
+                    Process[] app = Process.GetProcessesByName(ja[j].ToString());
+                    if (app.Length > 0) {
+                        for (int l = 0; l < app.Length; l++) {
+                            string fName = app[l].MainModule.FileName.Replace("\\", "/");
+                            if (fName == str + ".exe") {
+                                
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex) {
+
+                }
+            }
+        }
+    }
+}
